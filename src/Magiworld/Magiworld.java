b3869e15e8d096f8905794b799b3b41936272d48
FranckH <franckherbepin74@orange.fr>
2020-09-10 10:54:19 +0200

    suppression ligne 16 -17

--- src/Magiworld/Magiworld.java
+++ src/Magiworld/Magiworld.java
@@ -13,8 +13,7 @@ public class Magiworld {
 
         Combat.start(p1,p2);
 
-        System.out.println(p1.vie+" "+ p1.force+" "+p1.agilite+" "+p1.intelligence);
-        System.out.println(p2.vie+" "+ p2.force+" "+p2.agilite+" "+p2.intelligence);
+
     }
 
  }
